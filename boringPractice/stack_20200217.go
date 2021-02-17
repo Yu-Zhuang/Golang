2109a4954a221fe660895b3f3f6e83ba81b86f8c
Yu-Zhuang <106711129@gms.tcu.edu.tw>
2021-02-17 20:01:20 +0800

    stack implement

---
+++ boringPractice/stack_20200217.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type stack []int
+
+func (s *stack) push(v int) {
+	*s = append(*s, v)
+}
+
+func (s *stack) pop() (r int) {
+	r = (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return
+}
+
+func main() {
+	var s stack
+	fmt.Printf("stack value: %v ;; stack type: %T\n", s, s)
+
+	for i := 0; i < 10; i += 2 {
+		s.push(i)
+		fmt.Println(s)
+	}
+
+	fmt.Println(`====================`)
+
+	for i, j := range s {
+		fmt.Println(s.pop(), s)
+        i = i
+        j = j
+	}
+}
